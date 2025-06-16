b5be79eecbc1264afc2c16eac4c914a775d267f0
kadel237 <104805865+kadel237@users.noreply.github.com>
2025-06-16 11:34:23 +0200

    installation of the project

---
+++ cocktails/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `<h1>Hello cocktails !</h1>`,
+  styles: ``,
+})
+export class App {
+  protected title = 'cocktails';
+}
